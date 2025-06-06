6ba65820ef404ee1df650524146d8e9d29a24e85
ricky <rickyasuncion@outlook.com>
2025-06-06 13:47:54 -0700

    detect key inputs for movement

--- myScript.js
+++ myScript.js
@@ -9,51 +9,70 @@ let snake = [
   { x: 110, y: 150 },
 ];
 
+let dx = 10;
+let dy = 0;
+
 const drawSnakePart = (snakePart) => {
-  ctx.fillStyle = "lightgreen";
-  ctx.strokestyle = "darkgreen";
-  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.fillStyle = "lightgreen";
+    ctx.strokestyle = "darkgreen";
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 };
 
 const drawSnake = () => {
-  snake.forEach(drawSnakePart);
+    snake.forEach(drawSnakePart);
 };
 
 const clearCanvas = () => {
-  ctx.fillStyle = "white";
-  ctx.strokeStyle = "black";
-  ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-  ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 };
 
 const advanceSnake = (dx, dy) => {
-  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-  snake.unshift(head);
-  snake.pop();
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+    snake.unshift(head);
+    snake.pop();
 };
 
-const rightadvanceSnake = () => {
-  advanceSnake(10, 0);
+const changeDirection = (event) => {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+    
+    const keyPressed = event.keyCode;
+    
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+    
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+    }
+    if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+    }
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+    }
+    if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+    }
 };
 
-const leftadvanceSnake = () => {
-  advanceSnake(-10, 0);
-};
-
-const upadvanceSnake = () => {
-  advanceSnake(0, -10);
-};
-
-const downadvanceSnake = () => {
-  advanceSnake(0, 10);
-};
+document.addEventListener("keydown", changeDirection);
 
 const gameLoop = () => {
-  clearCanvas();
-  rightadvanceSnake();
-  drawSnake();
+    clearCanvas();
+    advanceSnake(dx, dy);
+    drawSnake();
 };
 
 setInterval(gameLoop, 100);
-
