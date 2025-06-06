4a9c6e34a5127e24a64724aff97daf37aeadf1cf
ricky <rickyasuncion@outlook.com>
2025-06-06 16:44:21 -0700

    create food for snek

--- myScript.js
+++ myScript.js
@@ -13,66 +13,99 @@ let dx = 10;
 let dy = 0;
 
 const drawSnakePart = (snakePart) => {
-    ctx.fillStyle = "lightgreen";
-    ctx.strokestyle = "darkgreen";
-    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+  ctx.fillStyle = "lightgreen";
+  ctx.strokestyle = "darkgreen";
+  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 };
 
 const drawSnake = () => {
-    snake.forEach(drawSnakePart);
+  snake.forEach(drawSnakePart);
 };
 
 const clearCanvas = () => {
-    ctx.fillStyle = "white";
-    ctx.strokeStyle = "black";
-    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
+  ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 };
 
 const advanceSnake = (dx, dy) => {
-    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-    snake.unshift(head);
-    snake.pop();
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(head);
+  snake.pop();
 };
 
+let changingDirection = false;
+
 const changeDirection = (event) => {
-    const LEFT_KEY = 37;
-    const RIGHT_KEY = 39;
-    const UP_KEY = 38;
-    const DOWN_KEY = 40;
-    
-    const keyPressed = event.keyCode;
-    
-    const goingUp = dy === -10;
-    const goingDown = dy === 10;
-    const goingRight = dx === 10;
-    const goingLeft = dx === -10;
-    
-    if (keyPressed === LEFT_KEY && !goingRight) {
-        dx = -10;
-        dy = 0;
-    }
-    if (keyPressed === UP_KEY && !goingDown) {
-        dx = 0;
-        dy = -10;
-    }
-    if (keyPressed === RIGHT_KEY && !goingLeft) {
-        dx = 10;
-        dy = 0;
-    }
-    if (keyPressed === DOWN_KEY && !goingUp) {
-        dx = 0;
-        dy = 10;
-    }
+  if (changingDirection) return;
+
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+
+  const keyPressed = event.keyCode;
+
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+
+  changingDirection = true;
 };
+
+const randomTen = (min, max) => {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+};
+
+let food = false;
+let foodX;
+let foodY;
+
+function createFood(food) {
+  foodX = randomTen(0, gameCanvas.width - 10);
+  foodY = randomTen(0, gameCanvas.height - 10);
+  snake.forEach(function isFoodOnSnake(part) {
+    const foodIsOnSnake = part.x == foodX && part.y == foodY;
+    if (foodIsOnSnake) createFood();
+  });
+}
+
+function drawFood() {
+  ctx.fillStyle = "red";
+  ctx.strokestyle = "darkred";
+  ctx.fillRect(foodX, foodY, 10, 10);
+  ctx.strokeRect(foodX, foodY, 10, 10);
+}
 
 document.addEventListener("keydown", changeDirection);
 
-const gameLoop = () => {
-    clearCanvas();
-    advanceSnake(dx, dy);
-    drawSnake();
+const main = () => {
+  clearCanvas();
+  drawFood();
+  advanceSnake(dx, dy);
+  drawSnake();
+  changingDirection = false;
 };
 
-setInterval(gameLoop, 100);
+createFood();
+setInterval(main, 100);
